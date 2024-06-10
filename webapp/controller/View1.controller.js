9243377f74f81e216db0ba5fea0c5f5bda47d302
Jefferson Fernandes Barreto <jeffinho_fb@hotmail.com>
2024-06-10 20:26:53 -0300

    Trabalhando com a View / Controller

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageToast) {
         "use strict";
 
         return Controller.extend("zjfbov.controller.View1", {
@@ -12,8 +13,23 @@ sap.ui.define([
 
             },
 
-            onPress: function(){
-                alert("Hello Word!!!!")
+            onCalcular: function(){
+                var oView = this.getView();
+                var iB1 = parseInt(oView.byId("b1").getValue());
+                var iB2 = parseInt(oView.byId("b2").getValue());
+                var iB3 = parseInt(oView.byId("b3").getValue());
+                var iB4 = parseInt(oView.byId("b4").getValue());
+                var fResultado = 0;
+                
+                fResultado = ( iB1 + iB2 + iB3 + iB4) / 4;
+
+                oView.byId("resultado").setValue(fResultado);
+
+                MessageToast.show("Resultado = "+fResultado);
+
+                // MessageToast
+                // sap.m.MessageToast
             }
+
         });
     });
